6d410a9eafa118eb4f3aadfd7e6187c132751d7e
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-04-22 22:47:56 +0530

    develop: api

---
+++ pages/agency/[id].tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function Dashboard() {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div className='p-12'>
+      <div>
+        <h1 className='text-xl font-bold'>Agency Name</h1>
+        <h1 className='text-xs font-semibold text-gray-400'>registered on 14-02-2021</h1>
+      </div>
+
+      <div className='flex space-x-5 my-4'>
+        <Link href={{
+          pathname: '/agency/[id]/addtickets',
+          query: { id: id }
+        }}>
+            <button className='text-green-500'>Add Tickets</button>
+        </Link>
+        <Link href={{
+          pathname: '/agency/[id]/removetickets',
+          query: { id: id }
+        }}>
+          <button className='text-rose-500'>Remove Tickets</button>
+        </Link>
+      </div>
+
+    </div>
+  )
+}
---
+++ pages/agency/[id]/removetickets.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function removetickets() {
+  return (
+    <div>removetickets</div>
+  )
+}
