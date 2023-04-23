4967950eff3129638999981a7646ef8c09c6aca4
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-04-23 16:01:00 +0530

    update: uploadtickets api

--- pages/agency/[id]/uploadtickets.tsx
+++ pages/agency/[id]/uploadtickets.tsx
@@ -21,12 +21,12 @@ export default function UploadTickets() {
     place: '',
     time: '',
   });
-  const [departureDate, setDepartureDate] = useState(null);
+  const [departureDate, setDepartureDate] = useState('');
 
   
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const data = { departFrom };
+    const data = { departFrom, goingTo, departureDate };
     const response = await fetch('/api/tickets/uploadtickets', {
       method: 'POST',
       headers: {
@@ -34,22 +34,44 @@ export default function UploadTickets() {
       },
       body: JSON.stringify(data)
     });
+    
+    const result = response.json();
+    console.log(result);
+    
     setDepartFrom({place: '', time: ''});
+    setGoingTo({place: '', time: ''});
+    setDepartureDate('');
   }
 
   return (
     <div className='p-12'>
-      <h1 className='font-semibold'>Upload Tickets</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          departFrom:
-          <input className='text-black' type="text" value={departFrom.place} placeholder='Place'
+      <h1 className='font-bold text-xl text-sky-500 mb-5'>Upload Tickets</h1>
+      <form className='flex flex-col space-y-4' onSubmit={handleSubmit}>
+        <label className='font-semibold'>
+          Depart From :
+          <input className='p-2 mr-5 rounded-xl mx-2 outline-none bg-neutral-800 font-bold' type="text" value={departFrom.place} placeholder='Place'
            onChange={(e) => setDepartFrom({...departFrom, place: e.target.value})}></input>
-          {/* <input className='text-black' type="time" value={departFrom.time} placeholder='Time' */}
-           {/* onChange={(e) => setDepartFrom({...departFrom, time: e.target.value})}></input> */}
+           <a className='font-semibold mr-2'>Time</a>
+          <input className='p-2 outline-none bg-neutral-800 rounded-xl' type="time" value={departFrom.time} placeholder='Time'
+            onChange={(e) => setDepartFrom({...departFrom, time: e.target.value})}></input>
         </label>
 
-        <button type="submit">Upload</button>
+        <label className='font-semibold ml-7'>
+          Going To :
+          <input className='p-2 mr-5 rounded-xl mx-2 outline-none bg-neutral-800 font-bold' type="text" value={goingTo.place} placeholder='Place'
+           onChange={(e) => setGoingTo({...goingTo, place: e.target.value})}></input>
+           <a className='font-semibold mr-2'>Time</a>
+          <input className='p-2 outline-none bg-neutral-800 rounded-xl' type="time" value={goingTo.time} placeholder='Time'
+            onChange={(e) => setGoingTo({...goingTo, time: e.target.value})}></input>
+        </label>
+
+        <label className='font-semibold'>
+          Departure Date :
+          <input className='p-2 outline-none bg-neutral-800 rounded-xl ml-2' type="date" value={departureDate} placeholder='Time'
+            onChange={(e) => setDepartureDate(e.target.value)}></input>
+        </label>
+
+        <button type="submit" className='bg-neutral-800 rounded-xl p-2 text-sky-500 font-bold hover:bg-opacity-80'>Upload</button>
       </form>
     </div>
   )
